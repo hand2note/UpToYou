c82fd1e76978b25d5a8aab646b0485d670ed6b82
AndrewPlakhotnyi <hand2note@gmail.com>
2021-09-26 08:44:11 -0700

    Fix. Backend.Runner doesn't work due to internalization of Client.dll

---
+++ tests/UpToYou.Core.Tests/Common/Proto.Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+using UpToYou.Core;
+namespace UpToYou.Core.Tests {
+public static class ProtoTests {
+    
+    [Test]
+    public static void 
+    SerializeVersion() =>
+        new Version("1.0.0.0").ProtoSerializeToBytes().DeserializeProto<Version>().DeepAssert(new Version("1.0.0.0"));
+}
+}
