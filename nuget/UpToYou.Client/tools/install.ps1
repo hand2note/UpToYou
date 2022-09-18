6c161b3ca54c00bdf037cfad63ecb91cc78cc87b
AndrewPlakhotnyi <hand2note@gmail.com>
2022-09-18 12:03:26 -0700

    Copy client.runner to output

--- nuget/UpToYou.Client/tools/install.ps1
+++ nuget/UpToYou.Client/tools/install.ps1
@@ -16,3 +16,7 @@ function MarkFileASCopyToOutputDirectory($item)
 
 MarkFileASCopyToOutputDirectory($project.ProjectItems.Item("liblzma.dll"))
 MarkFileASCopyToOutputDirectory($project.ProjectItems.Item("liblzma64.dll"))
+MarkFileASCopyToOutputDirectory($project.ProjectItems.Item("UpToYou.Client.Runner.dll"))
+MarkFileASCopyToOutputDirectory($project.ProjectItems.Item("UpToYou.Client.Runner.exe"))
+MarkFileASCopyToOutputDirectory($project.ProjectItems.Item("UpToYou.Client.Runner.deps.json"))
+MarkFileASCopyToOutputDirectory($project.ProjectItems.Item("UpToYou.Client.Runner.runtimeconfig.json"))
